03be8b1ba93bf802ab99f79e11bed6db4c5df9ca
limmg <limmg01@163.com>
2024-04-22 21:16:05 +0800

    修改底部和窗口其他链接

--- components/Common/Footer.vue
+++ components/Common/Footer.vue
@@ -6,10 +6,10 @@
 				<span
 					>Powered by
 					<a
-						href="https://www.fre123.com"
+						href="https://www.limmg.top/"
 						target="_blank"
 						class="decoration-transparent text-[#a4715c]"
-						>FRE123</a
+						>limmg</a
 					>
 				</span>
 			</div>
